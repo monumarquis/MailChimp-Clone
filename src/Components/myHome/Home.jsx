25c6fdc159770eb8935a84edee28c1a8e46820f2
vaibhavfirke <firkevaibhav11@gmail.com>
2022-11-13 16:38:09 +0530

    done

--- src/Components/myHome/Home.jsx
+++ src/Components/myHome/Home.jsx
@@ -32,7 +32,7 @@ import {
   AccordionPanel,
   AccordionIcon,
 } from "@chakra-ui/react";
-
+ 
 let Data = [
   {
     id: 1,
@@ -108,7 +108,7 @@ const Autometion2 = [
 ];
 
 function Home() {
-  // const [isHovering, setIsHovering] = useState(false);
+  
   const [gridData] = useState(Data);
   const [auto] = useState(Autometion1);
   const [auto2] = useState(Autometion2);
@@ -120,13 +120,7 @@ function Home() {
     "Generate up to 4x more orders* with Customer Journey Builder automations";
   const heading2 =
     "Get up to 88% more revenue* with our Customer Lifetime Value and Likelihood to Purchase segments";
-  // const handleMouseEnter = () => {
-  //   setIsHovering(true);
-  // };
-
-  // const handleMouseLeave = () => {
-  //   setIsHovering(false);
-  // };
+ 
 
   return (
     <Box w="100%" height="auto">
@@ -158,28 +152,7 @@ function Home() {
           Win new customers with the #1 email marketing and automations brand*
           that recomends ways to get more opens, clicks and sales.
         </Text>
-        {/* <Button
-          className="btn-signup"
-          display="block"
-          margin="auto"
-          mt={{ base: "16px", md: "18px", lg: "25px" }}
-          fontSize="13px"
-          borderRadius="50px"
-          w="120px"
-          h="50px"
-          border="1px solid black"
-          bg="#ffe01b"
-          style={{
-            borderBottom: isHovering ? "6px solid black" : "",
-            paddingBottom: isHovering ? "6px" : "",
-            background: isHovering ? "#ffe01b" : "",
-            transition: isHovering ? "0.3s" : "",
-          }}
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        >
-          Sign Up
-        </Button> */}
+     
         <Box w="120px" h="50px" borderRadius="50px" bg="black" margin="auto" mt="30px">
           <Button
             className="btn-signup"
